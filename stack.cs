4640cd1c86fe90fec67d113a2f7d6d6f3b34fde7
SimonKonstantinov <33290779+SimonKonstantinov@users.noreply.github.com>
2018-01-11 01:05:32 +0300

    это прочтейший стек
    какой же я раньше был глупый, я ведь этого не понимал. Почему обычный стек вызывал в моей голове столько страха? Я не знаю.
    Почему я не понимал этого раньше, а  сейчас это легко. Мне очень стыдно перед собой.

---
+++ stack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication1
+{
+    class stack
+    {
+        static void Main()
+        {
+
+            char y;
+            Console.WriteLine("Сколько элементов вы хотите создать?\n");
+
+            int x = int.Parse(Console.ReadLine());
+
+            var MyStack = new Stack<char>();
+
+            while (MyStack.Count < x)
+            {
+                Console.WriteLine("\nсоздайте элемент");
+                y = Console.ReadKey().KeyChar;
+                MyStack.Push(y);
+            }
+
+            Console.WriteLine("\nИсходный стек: ");
+            foreach (char s in MyStack)
+            {
+                Console.Write(s);
+            }
+            Console.WriteLine("\n");
+
+            while (MyStack.Count > 0)
+            {
+                Console.WriteLine("Pop -> {0}", MyStack.Pop());
+            }
+
+            if (MyStack.Count == 0)
+                Console.WriteLine("\nСтек пуст!");
+
+            Console.ReadLine();
+        }
+    }
+}
